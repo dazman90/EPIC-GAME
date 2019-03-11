d7def41f48caa88c9fe6c4868c04d1dd3b58ede1
Zig Zag <da.zeee.man@gmail.com>
2019-03-11 17:51:14 +0800

    Added playermovment and camera folow scripts
    Added FolowPlayer.cs and PlayerMovement.cs
    also capitalised name of Materials Directory

---
+++ Unity Root/EPIC GAME/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float WalkSpeed;
+    public float RunSpeed;
+    float CurrentSpeed;
+    public Transform Camera;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            CurrentSpeed = RunSpeed;
+        }
+        else
+        {
+            CurrentSpeed = WalkSpeed;
+        }
+        Vector3 CamF = Camera.forward;
+        Vector3 CamR = Camera.right;
+        CamF.y = 0f;
+        CamR.y = 0f;
+        CamF = CamF.normalized;
+        CamR = CamR.normalized;
+        transform.position += (CamR * Input.GetAxisRaw("Horizontal") *Time.deltaTime * CurrentSpeed + (CamF * Input.GetAxisRaw("Vertical")) * Time.deltaTime * CurrentSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+
+}
